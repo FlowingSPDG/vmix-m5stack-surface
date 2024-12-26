e90bcb7a8ebd024313e418603cfe55fc25e2aee0
Shugo Kawamura <ruk1ium@gmail.com>
2024-12-26 20:13:15 +0900

    Implement Tally

--- src/main.cpp
+++ src/main.cpp
@@ -2,56 +2,28 @@
 #include <WiFi.h>
 #include <Preferences.h>
 
+// type definitions
+// TODO: コマンドの共通化(class?)
+struct VMixCommandFunction {
+  String Function;
+  String Query;
+};
+
 // queue related
-QueueHandle_t xQueue = xQueueCreate( 10, sizeof( unsigned long ) );
+QueueHandle_t xQueueConnectWiFi = xQueueCreate( 1, sizeof( int8_t ) );
 QueueHandle_t xQueueConnectVMix = xQueueCreate( 1, sizeof( int8_t ) );
+QueueHandle_t xQueueVMixSendFunction = xQueueCreate( 1, sizeof( VMixCommandFunction ) );
 SemaphoreHandle_t preferencesSemaphore = xSemaphoreCreateMutex();
 SemaphoreHandle_t serialSemaphore = xSemaphoreCreateMutex();
 SemaphoreHandle_t spriteSemaphore = xSemaphoreCreateMutex();
+SemaphoreHandle_t clientSemaphore = xSemaphoreCreateMutex();
+SemaphoreHandle_t xSemaphoreWiFi = xSemaphoreCreateMutex();
 
 // instance
 WiFiClient client;
-
 Preferences preferences;
 
-static void TaskSender(void *pvParameters) {
-  auto coreID = xPortGetCoreID();
-  auto xLastWakeTime = xTaskGetTickCount();
-  BaseType_t xStatus;
-  int32_t SendValue = 0;
-
-  xSemaphoreTake(serialSemaphore, portMAX_DELAY);
-  M5.Lcd.fillScreen(TFT_BLUE);
-  xSemaphoreGive(serialSemaphore);
-  while (1) {
-    ++SendValue;
-    xStatus = xQueueSend(xQueue, &SendValue, 0);
-    if(xStatus != pdPASS){
-      xSemaphoreTake(serialSemaphore, portMAX_DELAY);
-      Serial.println("rtos queue send error...");
-      xSemaphoreGive(serialSemaphore);
-    }
-    vTaskDelayUntil(&xLastWakeTime, 1000 / portTICK_PERIOD_MS);
-  }
-}
-
-static void TaskReceiver(void *pvParameters) {
-  auto xLastWakeTime = xTaskGetTickCount();
-  BaseType_t xStatus;
-  int32_t ReceivedValue = 0;
-  
-  while (1) {
-    xStatus = xQueueReceive(xQueue, &ReceivedValue, portTICK_PERIOD_MS);
-    xSemaphoreTake(serialSemaphore, portMAX_DELAY);
-    if(xStatus == pdPASS){
-      Serial.printf("ReceivedValue:%d\n", ReceivedValue);
-    } else {
-      Serial.println("rtos queue receive error...");
-    }
-    xSemaphoreGive(serialSemaphore);
-    vTaskDelayUntil(&xLastWakeTime, 1000 / portTICK_PERIOD_MS);
-  }
-}
+// Task
 
 static void TaskConnectToWiFi(void *pvParameters) {
   auto xLastWakeTime = xTaskGetTickCount();
@@ -72,31 +44,56 @@ static void TaskConnectToWiFi(void *pvParameters) {
 
   int retry = 0;
   
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-  while (WiFi.status() != WL_CONNECTED){
-    delay(500);
-    xSemaphoreTake(spriteSemaphore, portMAX_DELAY);
-    M5.Lcd.print('.');
-    xSemaphoreGive(spriteSemaphore);
-    /*
-    if(++retry > 20){
-      xSemaphoreTake(spriteSemaphore, portMAX_DELAY);
-      M5.Lcd.println("Failed to connect to WiFi");
-      xSemaphoreGive(spriteSemaphore);
-
-      // QRコード表示タスクへ切り替える
-      
-      vTaskDelete(NULL);
-      return;
+  int8_t ReceivedValue = 0;
+  while(1){
+    if (xQueueReceive(xQueueConnectWiFi, &ReceivedValue, portMAX_DELAY) != pdPASS){
+      xSemaphoreTake(serialSemaphore, portMAX_DELAY);
+      Serial.println("Failed to receive queue");
+      xSemaphoreGive(serialSemaphore);
+      continue;
     }
-    */
-  }
-  xSemaphoreTake(spriteSemaphore, portMAX_DELAY);
-  M5.Lcd.println("Connected to WiFi");
-  xSemaphoreGive(spriteSemaphore);
-  int8_t SendValue = 0;
-  xQueueSend(xQueueConnectVMix, &SendValue, portMAX_DELAY);
-  vTaskDelete(NULL);
+
+    xSemaphoreTake(serialSemaphore, portMAX_DELAY);
+    Serial.println("Connecting to WiFi...");
+    xSemaphoreGive(serialSemaphore);
+
+    xSemaphoreTake(xSemaphoreWiFi, portMAX_DELAY);
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+    while (WiFi.status() != WL_CONNECTED){
+      xSemaphoreTake(serialSemaphore, portMAX_DELAY);
+      Serial.println("WiFi Retrying...");
+      xSemaphoreGive(serialSemaphore);
+
+      xSemaphoreTake(spriteSemaphore, portMAX_DELAY);
+      M5.Lcd.print('.');
+      xSemaphoreGive(spriteSemaphore);
+      /*
+      if(++retry > 20){
+        xSemaphoreTake(spriteSemaphore, portMAX_DELAY);
+        M5.Lcd.println("Failed to connect to WiFi");
+        xSemaphoreGive(spriteSemaphore);
+
+        // QRコード表示タスクへ切り替える
+
+        vTaskDelete(NULL);
+        return;
+      }
+      */
+     vTaskDelayUntil(&xLastWakeTime, 1000 / portTICK_PERIOD_MS);
+    }
+    xSemaphoreGive(xSemaphoreWiFi);
+
+    xSemaphoreTake(spriteSemaphore, portMAX_DELAY);
+    M5.Lcd.println("Connected to WiFi");
+    xSemaphoreGive(spriteSemaphore);
+
+    xSemaphoreTake(serialSemaphore, portMAX_DELAY);
+    Serial.println("Connected to WiFi");
+    xSemaphoreGive(serialSemaphore);
+
+    int8_t SendValue = 0;
+    xQueueSend(xQueueConnectVMix, &SendValue, portMAX_DELAY);
+  };
 }
 
 static void TaskConnectVMix(void *pvParameters) {
@@ -118,9 +115,22 @@ static void TaskConnectVMix(void *pvParameters) {
       xSemaphoreTake(serialSemaphore, portMAX_DELAY);
       Serial.println("Failed to receive queue");
       xSemaphoreGive(serialSemaphore);
-      return;
+      continue;
     }
+
+    xSemaphoreTake(serialSemaphore, portMAX_DELAY);
+    Serial.println("Connecting to vMix...");
+    xSemaphoreGive(serialSemaphore);
     
+    xSemaphoreTake(clientSemaphore, portMAX_DELAY);
+    if (client.connected()) {
+      xSemaphoreGive(clientSemaphore);
+
+      xSemaphoreTake(serialSemaphore, portMAX_DELAY);
+      Serial.println("Already connected to vMix");
+      xSemaphoreGive(serialSemaphore);
+      continue;
+    }
     while (!client.connect(VMIX_IP.c_str(), 8099)){
       xSemaphoreTake(serialSemaphore, portMAX_DELAY);
       Serial.println("Failed to connect to vMix");
@@ -138,7 +148,49 @@ static void TaskConnectVMix(void *pvParameters) {
     // Subscribe to the tally events
     client.println("SUBSCRIBE TALLY");
     client.println("SUBSCRIBE ACTS");
-    vTaskDelayUntil(&xLastWakeTime, 1000 / portTICK_PERIOD_MS);
+    xSemaphoreGive(clientSemaphore);
+  }
+}
+
+static void TaskVMixReceiveClient(void *pvParameters) {
+  auto xLastWakeTime = xTaskGetTickCount();
+  while (1) {
+    xSemaphoreTake(clientSemaphore, portMAX_DELAY);
+    if(!client.available()) {
+      xSemaphoreGive(clientSemaphore);
+      continue;
+    }
+    auto data = client.readStringUntil('\r\n');
+    xSemaphoreGive(clientSemaphore);
+
+    xSemaphoreTake(serialSemaphore, portMAX_DELAY);
+    Serial.printf("Received data from vMix: %s\n", data.c_str());
+    xSemaphoreGive(serialSemaphore);
+
+    // データ受信・パース処理
+    // 受け取ったデータをパースし、tally/activators専用のqueueに送信する
+    // vMixにデータを送るのは別タスクで行う
+  }
+}
+
+static void TaskVMixSendClient(void *pvParameters) {
+  auto xLastWakeTime = xTaskGetTickCount();
+  VMixCommandFunction ReceivedValue;
+  while (1) {
+    if (xQueueReceive(xQueueVMixSendFunction, &ReceivedValue, portMAX_DELAY) != pdPASS){
+      xSemaphoreTake(serialSemaphore, portMAX_DELAY);
+      Serial.println("Failed to receive queue");
+      xSemaphoreGive(serialSemaphore);
+      continue;
+    }
+    // これはTCP APIに接続されてるかどうかの確認であっているのか？
+    xSemaphoreTake(clientSemaphore, portMAX_DELAY);
+    if (client.connected()) {
+      // データ送信処理
+      // tally/activators専用のqueueからデータを受け取り、vMixに送信する
+      client.printf("%s %s\r\n", ReceivedValue.Function.c_str(), ReceivedValue.Query.c_str());
+    }
+    xSemaphoreGive(clientSemaphore);
   }
 }
 
@@ -148,14 +200,13 @@ void setup() {
   auto cfg = M5.config();
   M5.begin(cfg);
 
-  preferences.begin("vMixTally", false);
-  preferences.putString("vmix_ip", "192.168.1.6");
-  preferences.end();
-
-  xTaskCreatePinnedToCore(TaskSender, "Sender", 4096, NULL, 1,NULL, 0);
-  xTaskCreatePinnedToCore(TaskReceiver, "Receiver", 4096, NULL, 1,NULL, 1);
-  xTaskCreatePinnedToCore(TaskConnectToWiFi, "ConnectToWiFi", 4096, NULL, 1,NULL, 0);
+  xTaskCreatePinnedToCore(TaskConnectToWiFi, "ConnectToWiFi", 4096, NULL, 1,NULL, 1);
   xTaskCreatePinnedToCore(TaskConnectVMix, "ConnectVMix", 4096, NULL, 1,NULL, 1);
+  xTaskCreatePinnedToCore(TaskVMixReceiveClient, "VMixReceiveClient", 4096, NULL, 1,NULL, 1);
+  // xTaskCreatePinnedToCore(TaskVMixSendClient, "VMixSendClient", 4096, NULL, 1,NULL, 1);
+
+  int8_t SendValue = 0;
+  xQueueSend(xQueueConnectWiFi, &SendValue, portMAX_DELAY);
 }
 
 void loop() {}
